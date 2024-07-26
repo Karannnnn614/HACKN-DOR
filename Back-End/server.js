9a209b70e67501b39ba3f5973b8b59b6337a1934
Karan Mundre <karanmundre73@gmail.com>
2024-07-27 05:06:46 +0530

    backend initialise

--- Back-End/server.js
+++ Back-End/server.js
@@ -4,6 +4,6 @@ const app = express();
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to express");
+    res.send("Welcome to asjfjghas");
 })
 console.log("server invoked at link http://localhost:3000");
