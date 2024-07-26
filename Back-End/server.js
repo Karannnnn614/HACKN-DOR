b40a7728508540343a562b7a4c73dc7d1f6839db
Karan Mundre <karanmundre73@gmail.com>
2024-07-27 04:56:47 +0530

    Modifying Server file

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,4 +2,8 @@ import express from 'express';
  
 const app = express();
 app.listen(3000);
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to express");
+})
 console.log("server invoked at link http://localhost:3000");
